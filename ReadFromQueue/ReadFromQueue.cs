f7e1b0ab239b96a5083f49418c84c67c11c2139b
Vivek Jayachandran <vivek4integration@gmail.com>
2022-06-19 20:25:58 +0530

    Initial Commit

---
+++ ReadFromQueue/ReadFromQueue.cs
@@ -0,0 +1,52 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace ReadFromQueue
+{
+    public static class ReadFromQueue
+    {
+        [FunctionName("ReadFromQueue")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            // using Azure.Messaging.ServiceBus;
+
+            string connectionString = System.Environment.GetEnvironmentVariable("QueueConnection");
+            string queueName = System.Environment.GetEnvironmentVariable("QueueName");
+
+            
+
+            // Because ServiceBusClient implements IAsyncDisposable, we'll create it 
+            // with "await using" so that it is automatically disposed for us.
+            await using var client = new ServiceBusClient(connectionString);
+            
+
+            // The receiver is responsible for reading messages from the queue.
+            ServiceBusReceiver receiver = client.CreateReceiver(queueName);
+            ServiceBusReceivedMessage receivedMessage = await receiver.ReceiveMessageAsync();
+
+
+            string body = string.Empty; 
+            if (receivedMessage != null)
+            body = receivedMessage.Body.ToString();
+            Console.WriteLine(body);
+
+            string responseMessage = string.IsNullOrEmpty(body)
+                ? "Body is empty"
+                : body;
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
